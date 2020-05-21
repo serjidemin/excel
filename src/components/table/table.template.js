90096707865cb5268bd7554366a3235632265396
Sergey Demin <serji2003@rambler.ru>
2020-05-21 23:21:16 +0500

    Finish base framework functional (#3)

---
+++ src/components/table/table.template.js
@@ -0,0 +1,53 @@
+const charCodes = {
+  A: 65,
+  Z: 90,
+};
+
+function toCell() {
+  return `
+    <div class="cell" contenteditable></div>
+  `;
+}
+
+function toColumn(col) {
+  return `
+    <div class="column">${col}</div>
+  `;
+}
+
+function createRow(index, content) {
+  return `
+    <div class="row">
+      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-data">${content}</div>
+    </div>
+  `;
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(charCodes.A + index);
+}
+
+export function createTable(rowsCount = 15) {
+  const colsCount = charCodes.Z - charCodes.A + 1;
+  const rows = [];
+
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(el => toColumn(el))
+      .join('');
+
+  rows.push(createRow(null, cols));
+
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('');
+
+    rows.push(createRow(i + 1, cells));
+  }
+
+  return rows.join('');
+}
